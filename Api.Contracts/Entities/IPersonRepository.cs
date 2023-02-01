2ef89a287256259e9de3a805638ed2c5f3c618b9
Luis Carlos Salas Villalobos <lusalas16@gmail.com>
2023-01-31 19:38:45 -0600

    ejemplo

---
+++ Api.Contracts/Entities/IPersonRepository.cs
@@ -0,0 +1,13 @@
+﻿using Api.Entities;
+
+namespace Api.Contracts
+{
+    public interface IPersonRepository : IRepositoryBase<Person>
+    {
+        IEnumerable<Person> GetAllPersons();
+        Person GetPersonById(int personsId);
+        void CreatePerson(Person person);
+        void UpdatePerson(Person person);
+        void DeletePerson(Person person);
+    }
+}
---
+++ Api.Repository/Entities/PersonRepository.cs
@@ -0,0 +1,41 @@
+﻿using Api.Contracts;
+using Api.Entities;
+
+namespace Api.Repository
+{
+    public class PersonRepository : RepositoryBase<Person>, IPersonRepository
+    {
+        public PersonRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+        }
+
+        public IEnumerable<Person> GetAllPersons()
+        {
+            return FindAll()
+                .OrderBy(o => o.FullName)
+                .ToList();
+        }
+
+        public Person GetPersonById(int personId)
+        {
+            return FindByCondition(o => o.Id.Equals(personId))
+                    .FirstOrDefault();
+        }
+
+        public void CreatePerson(Person person)
+        {
+            Create(person);
+        }
+
+        public void UpdatePerson(Person person)
+        {
+            Update(person);
+        }
+
+        public void DeletePerson(Person person)
+        {
+            Delete(person);
+        }
+    }
+}
